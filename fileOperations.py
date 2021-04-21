deaad56b596f7aedda65e8dd65390fb090927904
Connor <ConnorKirkpatrick3@gmail.com>
2021-04-21 15:46:57 +0100

    Added code to automatically set up a guild file if there is not one already created

--- fileOperations.py
+++ fileOperations.py
@@ -1,60 +1,129 @@
-def getChannel():
-    values = open("values.txt", "r")
-    content = values.readlines()
+import threading
+
+def setupFile(guild):
+    guild = str(guild)
+    values = open("guildSettings/"+guild+".txt", "w")
+    values.write("channel:,ServerName:,monitorFlag:0,guild:")
     values.close()
-    channel = content[0].split(",")[0].split(":")[1]
-    return channel
 
 
-def setChannel(channel):
+def getChannel(guild):
+    guild = str(guild)
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open("guildSettings/"+guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        channel = content[0].split(",")[0].split(":")[1]
+        return channel
+
+
+def setChannel(channel, guild):
+    guild = str(guild)
     channel = channel.strip()
-    values = open("values.txt", "r")
-    content = values.readlines()
-    values.close()
-    content = content[0].split(",")
-    new = "channel:" + channel + "," + content[1] + "," + content[2] + ","
-    f = open("values.txt", "w")
-    f.write(new)
-    f.close()
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open("guildSettings/"+guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        content = content[0].split(",")
+        new = "channel:" + channel + "," + content[1] + "," + content[2] + ","
+        f = open("guildSettings/"+guild+".txt", "w")
+        f.write(new)
+        f.close()
 
 
-def getServer():
-    values = open("values.txt", "r")
-    content = values.readlines()
-    values.close()
-    server = content[0].split(",")[1].split(":")[1]
-    return server
+def getServer(guild):
+    guild = str(guild)
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open("guildSettings/"+guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        server = content[0].split(",")[1].split(":")[1]
+        return server
 
 
-def setServer(Server):
+def setServer(Server, guild):
+    guild = str(guild)
     Server = Server.strip()
-    values = open("values.txt", "r")
-    content = values.readlines()
-    values.close()
-    content = content[0].split(",")
-    new = content[0] + ",ServerName:" + Server + "," + content[2] + ","
-    f = open("values.txt", "w")
-    f.write(new)
-    f.close()
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open(guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        content = content[0].split(",")
+        new = content[0] + ",ServerName:" + Server + "," + content[2] + ","
+        f = open("guildSettings/"+guild+".txt", "w")
+        f.write(new)
+        f.close()
 
 
-def getFlag():
-    values = open("values.txt", "r")
-    content = values.readlines()
-    values.close()
-    flag = content[0].split(",")[2].split(":")[1]
-    return flag
+def getFlag(guild):
+    guild = str(guild)
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open("guildSettings/"+guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        flag = content[0].split(",")[2].split(":")[1]
+        return flag
 
 
-def setFlag(flag):
-    values = open("values.txt", "r")
-    content = values.readlines()
-    values.close()
-    content = content[0].split(",")
-    new = content[0] + ","+content[1]+",monitorFlag:" + str(flag) + ","
-    f = open("values.txt", "w")
-    f.write(new)
-    f.close()
+def setFlag(flag, guild):
+    guild = str(guild)
+    values = ""
+    try:
+        values = open("guildSettings/"+guild+".txt", "r")
+    except Exception as e:
+        print(e)
+        setup = threading.Thread(target=setupFile, args=(guild,))
+        setup.start()
+        setup.join()
+    finally:
+        values = open("guildSettings/"+guild+".txt", "r")
+        content = values.readlines()
+        values.close()
+        content = content[0].split(",")
+        new = content[0] + ","+content[1]+",monitorFlag:" + str(flag) + ","
+        f = open("guildSettings/"+guild+".txt", "w")
+        f.write(new)
+        f.close()
 
 def getGuild():
     values = open("values.txt", "r")
@@ -64,6 +133,7 @@ def getGuild():
     return guild
 
 def setGuild(guild):
+    guild = str(guild)
     values = open("values.txt", "r")
     content = values.readlines()
     values.close()
--- main.py
+++ main.py
@@ -6,39 +6,50 @@ import threading
 import fileOperations
 import monitorTimer
 
+
 class MyClient(discord.Client):
 
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
-        #await monitorLoop.getDetails(client)
-
-
+        # await monitorLoop.getDetails(client)
 
     async def on_message(self, message):
         if message.author == client.user:
             return
         else:
             if message.content.__contains__("kirk-help"):
-                await message.reply("Use Commands wit hthe prefix 'kirk-':\nsetChannel: set the channel the monitor will post in\nsetServer: give the full name of the server you wish to monitor\nstartMonitor: Start the monito\nstopMonitor: stop the monitor\nhelp:Display this message")
+                await message.reply("Use Commands wit hthe prefix 'kirk-':\n"
+                                    "setChannel: set the channel the monitor will post in\n"
+                                    "setServer: give the full name of the server you wish to monitor\n"
+                                    "startMonitor: Start the monitor\n"
+                                    "stopMonitor: stop the monitor\nhelp:Display this message")
             elif message.content.__contains__("kirk-setChannel"):
                 await message.reply("setting chan")
-                fileOperations.setChannel(message.content[15:])
+                fileOperations.setChannel(message.content[15:], message.guild)
             elif message.content.__contains__("kirk-setServer"):
                 await message.reply("setting ser")
-                fileOperations.setServer(message.content[14:])
+                fileOperations.setServer(message.content[14:], message.guild)
             elif message.content.__contains__("kirk-startMonitor"):
                 await message.reply("Setting up.......")
-                fileOperations.setFlag(1)
+                fileOperations.setFlag(1, message.guild)
                 time.sleep(2.5)
-                await monitorTimer.monitorTimer(client)
+                await monitorTimer.monitorTimer(client, message.guild)
             elif message.content.__contains__("kirk-stopMonitor"):
                 await message.reply("stopping.....")
-                fileOperations.setFlag(0)
+                fileOperations.setFlag(0, message.guild)
             elif message.content.__contains__("kirk-status"):
-                await message.reply("Server: "+fileOperations.getServer()+"\nChannel: "+fileOperations.getChannel()+"\nFlag: "+fileOperations.getFlag())
+                await message.reply("Server: " + fileOperations.getServer(message.guild) + "\nChannel: " +
+                                    fileOperations.getChannel(message.guild) + "\nFlag: " +
+                                    fileOperations.getFlag(message.guild))
+
 
 client = MyClient()
 client.run(os.environ["BOT-TOKEN"])
 print(threading.current_thread())
 
+
+#idea, updater threat always running on 30 sec intervals
+#checks global array for server names to monitor and guild names
+#pushes the details to the guild files
+#guild files wait for that write event, when triggered they read and output
