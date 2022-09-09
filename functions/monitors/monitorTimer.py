e858c9bdc7ca611b578d9cbae16160757634e591
Connor <ConnorKirkpatrick3@gmail.com>
2022-09-09 14:56:02 +0100

    Updated delay time to 60 seconds from 30

--- functions/monitors/monitorTimer.py
+++ functions/monitors/monitorTimer.py
@@ -7,7 +7,7 @@ async def monitorTimer(client, guild, message, monitor):
     while True:
         if fileOperations.getFlag(str(guild), monitor) == "1\n":
             status = await monitorLoop.getDetails(client, str(guild), message, monitor)
-            await asyncio.sleep(30)
+            await asyncio.sleep(60)
             if status != 1:
                 break
         else:
