5994de79511632270326352042d17644d356ae81
Connor <ConnorKirkpatrick3@gmail.com>
2022-09-11 16:44:48 +0100

    Added fetch loop method, fetched new data from the ED website every 60 seconds if atleast 1 monitor is running

---
+++ functions/monitors/fetchLoop.py
@@ -0,0 +1,8 @@
+import asyncio
+import serverList
+
+async def fetchLoop(object):
+    while True:
+        if object.runningMonitors > 0:
+            object.servers = serverList.getServerList()
+        await asyncio.sleep(10)
